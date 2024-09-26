c589d8b826879060260f856e86fb916a2b6bdf60
Robbie1054 <92984438+Robbie1054@users.noreply.github.com>
2024-09-26 15:03:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-url = 'https://www.youtube.com/watch?v=5YytKT8Z8W8'
-print(url)
+def get_url():
+    data = request.json
+    url = data.get('url')
+    url_format(url)
 
 def url_format(url):
   video_id = url.replace('https://www.youtube.com/watch?v=', '')
@@ -16,11 +18,7 @@ def get_transcript(video_id):
   get_output(transcript)
 
 def get_output(transcript):
-  output=''
-  for x in transcript:
-    sentence = x['text']
-    output += f' {sentence}\n'
-  print(output)
+  return jsonify(transcript)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
